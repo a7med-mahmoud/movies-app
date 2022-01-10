096f57ab5eb220b9fa4689af1067f0f4c2f297f2
Ahmed Mahmoud <a7med.mahmoud2004@gmail.com>
2022-01-10 08:37:26 +0200

    feat: set tabbar icons

--- src/navigation/HomeNavigator.tsx
+++ src/navigation/HomeNavigator.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 import AllMoviesScreen from '../screens/AllMoviesScreen';
 
@@ -8,8 +9,30 @@ const Tab = createBottomTabNavigator();
 const HomeNavigator: React.FC = () => {
   return (
     <Tab.Navigator>
-      <Tab.Screen name="All Movies" component={AllMoviesScreen} />
-      <Tab.Screen name="My Movies" component={AllMoviesScreen} />
+      <Tab.Screen
+        name="All Movies"
+        component={AllMoviesScreen}
+        options={{
+          tabBarIcon: props => (
+            <Ionicons
+              {...props}
+              name={props.focused ? 'film' : 'film-outline'}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="My Movies"
+        component={AllMoviesScreen}
+        options={{
+          tabBarIcon: props => (
+            <Ionicons
+              {...props}
+              name={props.focused ? 'person' : 'person-outline'}
+            />
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 };
