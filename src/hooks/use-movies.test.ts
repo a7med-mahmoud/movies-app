45b0a84eef61f3605fe7e3375ee45b385433ca35
Ahmed Mahmoud <a7med.mahmoud2004@gmail.com>
2022-01-11 06:22:46 +0200

    test: add `nextPage` to tests of `useMovies` reducer

--- src/hooks/use-movies.test.ts
+++ src/hooks/use-movies.test.ts
@@ -35,7 +35,7 @@ describe('Movies Reducer', () => {
     expect(state).toEqual(initialState);
   });
 
-  it('sets movies and resets loading and error on fetch success', () => {
+  it('sets movies and resets loading/error on fetch success', () => {
     const state = reducer(undefined, {
       type: FETCH_SUCCESS,
       payload: SAMPLE_MOVIES,
@@ -44,11 +44,48 @@ describe('Movies Reducer', () => {
     expect(state.isLoading).toBe(false);
     expect(state.movies).toEqual(SAMPLE_MOVIES);
     expect(state.error).toBeNull();
+    expect(state.nextPage).toBe(2);
+  });
+
+  it('adds to movies and resets loading/error on fetch success with existing movies', () => {
+    const NEW_MOVIES = [
+      {
+        id: 4,
+        title: 'movie 4',
+        overview: 'overview',
+        release_date: '1990-06-02',
+      },
+    ];
+
+    const state = reducer(
+      {
+        error: null,
+        isLoading: true,
+        movies: SAMPLE_MOVIES,
+        nextPage: 2,
+      },
+      {
+        type: FETCH_SUCCESS,
+        payload: [
+          {
+            id: 4,
+            title: 'movie 4',
+            overview: 'overview',
+            release_date: '1990-06-02',
+          },
+        ],
+      },
+    );
+
+    expect(state.isLoading).toBe(false);
+    expect(state.movies).toEqual([...SAMPLE_MOVIES, ...NEW_MOVIES]);
+    expect(state.nextPage).toBe(3);
+    expect(state.error).toBeNull();
   });
 
   it('sets error and resets loading only on fetch failure', () => {
     const state = reducer(
-      { movies: SAMPLE_MOVIES, isLoading: true, error: null, nextPage: 1 },
+      { movies: SAMPLE_MOVIES, isLoading: true, error: null, nextPage: 2 },
       {
         type: FETCH_FAILURE,
         payload: 'Some error',
@@ -58,16 +95,18 @@ describe('Movies Reducer', () => {
     expect(state.isLoading).toBe(false);
     expect(state.movies).toEqual(SAMPLE_MOVIES);
     expect(state.error).toBe('Some error');
+    expect(state.nextPage).toBe(2);
   });
 
   it('sets loadings and resets error on fetch init', () => {
     const state = reducer(
-      { movies: SAMPLE_MOVIES, isLoading: true, error: null, nextPage: 1 },
+      { movies: SAMPLE_MOVIES, isLoading: true, error: null, nextPage: 2 },
       { type: FETCH_INIT },
     );
 
     expect(state.isLoading).toBe(true);
     expect(state.movies).toEqual(SAMPLE_MOVIES);
-    expect(state.error).toBe(null);
+    expect(state.error).toBeNull();
+    expect(state.nextPage).toBe(2);
   });
 });
