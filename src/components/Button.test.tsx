e840bb9dd848f96ced234b22f814f0f6f6b1c236
Ahmed Mahmoud <a7med.mahmoud2004@gmail.com>
2022-01-12 23:14:53 +0200

    test: Button component

---
+++ src/components/Button.test.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { fireEvent, render } from '@testing-library/react-native';
+
+import Button from './Button';
+
+describe('Button', () => {
+  it('renders text correctly', () => {
+    const { getByText } = render(<Button>I'm a Button</Button>);
+
+    expect(getByText("I'm a Button")).toBeTruthy();
+  });
+
+  it('handles press event successfully', () => {
+    const onPress = jest.fn();
+    const { container } = render(<Button onPress={onPress}>Button</Button>);
+
+    fireEvent.press(container);
+
+    expect(onPress).toBeCalled();
+  });
+});
