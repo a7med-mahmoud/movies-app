0a3173a9f497b019d186c7d78afa99fb17cb34da
Ahmed Mahmoud <a7med.mahmoud2004@gmail.com>
2022-01-12 23:37:53 +0200

    test: `AddMovie` todos

---
+++ src/components/AddMovie.test.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import AddMovie from './AddMovie';
+
+describe('AddMovie', () => {
+  it.todo('shows add movie modal on add button press');
+  it.todo('hides add movie modal on overlay press');
+  it.todo('hides add movie modal on add finishes');
+});
