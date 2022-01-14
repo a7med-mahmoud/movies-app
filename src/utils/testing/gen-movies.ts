c0ef1e8649fe359e5f36865769676c4d5ff4e0af
Ahmed Mahmoud <a7med.mahmoud2004@gmail.com>
2022-01-14 13:31:58 +0200

    chore: use `nanoid` for movie id in `genMovies`

--- src/utils/testing/gen-movies.ts
+++ src/utils/testing/gen-movies.ts
@@ -1,10 +1,11 @@
 import faker from 'faker';
+import { nanoid } from 'nanoid';
 
 import type Movie from '../../types/movie';
 
 function genMovies(count = 3): Movie[] {
   return new Array(count).fill(null).map(() => ({
-    id: Math.random(),
+    id: nanoid(),
     title: faker.name.title(),
     overview: faker.lorem.lines(3),
     release_date: faker.date.past().toDateString(),
