b1bf2038ab5213b58d6b63222cb42bff747a0ac0
Ahmed Mahmoud <a7med.mahmoud2004@gmail.com>
2022-01-25 06:59:03 +0200

    chore: type MoviesNavigator with ParamList

--- src/navigation/MoviesNavigator.tsx
+++ src/navigation/MoviesNavigator.tsx
@@ -10,7 +10,7 @@ export type MoviesParamList = {
   MovieDetails: { movie: Movie };
 };
 
-const Stack = createSharedElementStackNavigator();
+const Stack = createSharedElementStackNavigator<MoviesParamList>();
 
 const MoviesNavigator = () => {
   return (
